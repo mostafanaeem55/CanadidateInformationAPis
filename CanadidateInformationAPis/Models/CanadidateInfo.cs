0e68bd45e37931fcc310955dd6108b4baede6904
Mostafa Naeem <mostafanaeem55@gmail.com>
2024-05-10 19:18:54 +0300

    Add project files.

---
+++ CanadidateInformationAPis/Models/CanadidateInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CanadidateInformationAPis.Models
+{
+    public class CanadidateInfo
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public string BestCallTime { get; set; }
+
+        public string LinkedInURL { get; set; }
+
+        public string GitHubURL { get; set; }
+
+        [Required]
+        public string Comment { get; set; }
+
+    }
+}
