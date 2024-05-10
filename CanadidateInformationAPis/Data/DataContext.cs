07d15eb79a2e1e8dc01125545080720710a6122f
Mostafa Naeem <mostafanaeem55@gmail.com>
2024-05-10 19:46:18 +0300

    Adding The DB Context

---
+++ CanadidateInformationAPis/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using CanadidateInformationAPis.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CanadidateInformationAPis.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<CanadidateInfo> canadidateInfos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CanadidateInfo>()
+                .HasKey(c => c.Email);
+
+        }
+    }
+}
