fc4b36013a10e1b84ba69dafa3f7bc7e25e19d25
Mostafa Naeem <mostafanaeem55@gmail.com>
2024-05-11 20:55:06 +0300

    Add Repository

---
+++ CanadidateInformationAPis/Interfaces/ICandidateRepository.cs
@@ -0,0 +1,12 @@
+﻿using CanadidateInformationAPis.Models;
+
+namespace CanadidateInformationAPis.Interfaces
+{
+    public interface ICandidateRepository
+    {
+        Task<CanadidateInfo> GetCandidateByEmail(string email);
+        Task AddCandidate(CanadidateInfo candidate);
+        Task UpdateCandidate(CanadidateInfo candidate);
+        Task SaveChangesAsync();
+    }
+}
---
+++ CanadidateInformationAPis/Repository/CanadidateRepository.cs
@@ -0,0 +1,37 @@
+﻿using CanadidateInformationAPis.Data;
+using CanadidateInformationAPis.Interfaces;
+using CanadidateInformationAPis.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CanadidateInformationAPis.Repository
+{
+    public class CanadidateRepository : ICandidateRepository
+    {
+        private readonly DataContext _context;
+
+        public CanadidateRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CanadidateInfo> GetCandidateByEmail(string email)
+        {
+            return await _context.canadidateInfos.FirstOrDefaultAsync(c => c.Email == email);
+        }
+
+        public async Task AddCandidate(CanadidateInfo candidate)
+        {
+            await _context.canadidateInfos.AddAsync(candidate);
+        }
+
+        public Task UpdateCandidate(CanadidateInfo candidate)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task SaveChangesAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+    }
+}
