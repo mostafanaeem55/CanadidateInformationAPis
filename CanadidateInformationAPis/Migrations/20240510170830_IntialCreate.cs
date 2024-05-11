969a7296a8324c74dfd4dce1d5e57d3a2cc52a91
Mostafa Naeem <mostafanaeem55@gmail.com>
2024-05-11 20:55:26 +0300

    First Migration

---
+++ CanadidateInformationAPis/Migrations/20240510170830_IntialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CanadidateInformationAPis.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "canadidateInfos",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BestCallTime = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LinkedInURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GitHubURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_canadidateInfos", x => x.Email);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "canadidateInfos");
+        }
+    }
+}
